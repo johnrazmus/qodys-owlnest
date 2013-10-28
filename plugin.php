9532b59c967d91eabb4b5d3a16e2fff0f9a88d80
John Razmus <john@qody.co>
2013-10-28 10:52:34 -0500

    Updated to version 3.9.3

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Qody's Owl Nest
  * Plugin URI: http://qody.co
  * Description: Framework required for all plugins managed by Qody's owls.
- * Version: 3.9.2
+ * Version: 3.9.3
  * Author: Qody LLC
  * Author URI: http://qody.co
  *
